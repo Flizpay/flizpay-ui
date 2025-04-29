1c37ac0183c47abbe20706a425caca1db3439551
Michel Mendes <m1tchofc@gmail.com>
2025-04-29 15:39:55 +0100

    fix: styles

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,30 @@
 import { ButtonHTMLAttributes } from 'react';
 import { DetailedHTMLProps } from 'react';
 import { ForwardRefExoticComponent } from 'react';
+import { JSX } from 'react';
 import { RefAttributes } from 'react';
 
+export declare function BusinessFAQ({ language, className, topic }: BusinessFAQProps): JSX.Element;
+
+declare interface BusinessFAQProps extends FAQProps {
+    topic: BusinessTopic;
+}
+
+declare type BusinessTopic = "payment" | "general" | "instructions" | "security";
+
 export declare const Button: ForwardRefExoticComponent<Omit<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, "ref"> & RefAttributes<HTMLButtonElement>>;
+
+declare interface FAQProps {
+    className?: string;
+    language: string;
+}
+
+export declare function ShopperFAQ({ language, className, topic }: ShopperFAQProps): JSX.Element;
+
+declare interface ShopperFAQProps extends FAQProps {
+    topic: ShopperTopic;
+}
+
+declare type ShopperTopic = "paying" | "cashback" | "security";
 
 export { }
