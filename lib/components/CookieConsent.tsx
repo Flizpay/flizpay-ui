dfdd1d150982c35b3cd5fe643003b53ae70746c8
OlegSyng <olegsingaevskii9@gmail.com>
2025-05-19 17:25:37 +0300

    feat: release to npm

--- lib/components/CookieConsent.tsx
+++ lib/components/CookieConsent.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useCallback, useEffect, useState } from "react";
 import { Button } from "@/components/Button";
 import * as enCookieConsent from "@/locales/en/cookie-consent/en.json";
